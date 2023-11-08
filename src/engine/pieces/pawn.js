ae518f678483e1cf3653e1b48c76e3508264556d
kellyfragk <kelly.fragkogianni@softwire.com>
2023-11-08 16:12:16 +0000

    1st group of tests

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,11 +1,19 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../../../src/engine/square";
+import Player from "../../../src/engine/player";
 
 export default class Pawn extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+  getAvailableMoves(board) {
+    let position;
+    if (this.player === Player.WHITE) {
+      position = new Array(Square.at(1, 0));
+    } else {
+      position = new Array(Square.at(6, 7));
     }
+    return position;
+  }
 }
